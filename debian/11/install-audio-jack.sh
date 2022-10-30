9d7d937c00a74701644b23eebfac8ff68e165125
Brendan Ingram <hello@brendaningram.com>
2022-10-31 10:09:14 +1100

    Updated REAPER version to 6.69.

--- debian/11/install-audio-jack.sh
+++ debian/11/install-audio-jack.sh
@@ -95,7 +95,7 @@ read -p "Would you like to install REAPER (Y/N)? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  wget -O reaper.tar.xz http://reaper.fm/files/6.x/reaper668_linux_x86_64.tar.xz
+  wget -O reaper.tar.xz http://reaper.fm/files/6.x/reaper669_linux_x86_64.tar.xz
   mkdir ./reaper
   tar -C ./reaper -xf reaper.tar.xz
   ./reaper/reaper_linux_x86_64/install-reaper.sh --install ~/ --integrate-desktop
