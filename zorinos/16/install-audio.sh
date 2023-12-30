cb1e786c4cabd661b59199940c89da8bfb7c4caa
Jonas Wiberg <jonas.wiberg@stabelo.se>
2023-12-30 15:57:55 +0100

    Update REAPER to 7.07

--- zorinos/16/install-audio.sh
+++ zorinos/16/install-audio.sh
@@ -91,7 +91,7 @@ sudo adduser $USER audio
 # at ~/REAPER.
 # ---------------------------
 notify "REAPER"
-wget -O reaper.tar.xz http://reaper.fm/files/6.x/reaper683_linux_x86_64.tar.xz
+wget -O reaper.tar.xz http://reaper.fm/files/7.x/reaper707_linux_x86_64.tar.xz
 mkdir ./reaper
 tar -C ./reaper -xf reaper.tar.xz
 ./reaper/reaper_linux_x86_64/install-reaper.sh --install ~/ --integrate-desktop
